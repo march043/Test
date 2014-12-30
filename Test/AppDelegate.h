43e14b06727820dbdeb46b6d25d36f8875131aa1
seongmoo <seongmoo@me.com>
2014-12-30 13:52:06 +0900

    장난

--- Test/AppDelegate.h
+++ Test/AppDelegate.h
@@ -11,11 +11,8 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-///
-///
-//
-//
 
+@property (strong, nonatomic) UIImage *sampleImage;
 
 @end
 
